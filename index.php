5662646409e023639f884dd56739099a816f0d63
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-12-14 15:01:26 +0100

    CREAZIONE VARIABILE E PRINT

--- index.php
+++ index.php
@@ -10,40 +10,47 @@
 
 <body>
 
-    <?php
-        $stray_heart_lyrics = 'I lost my way, oh baby, this stray heart
-        Went to another, can you recover, baby?
-        Oh, you\'re the only one that I\'m dreamin\' of
-        Your precious heart was torn apart by me and you
-        You\'re not alone, oh, and now I\'m where I belong
-        We\'re not alone, oh, I\'ll hold your heart and never let go
-        Everything that I want, I want from you
-        But I just can\'t have you
-        Everything that I need, I need from you
-        But I just can\'t have you
-        I said a thousand times and now a thousand one
-        We\'ll never part, I\'ll never stray again from you
-        This dog is destined for a home to your heart
-        We\'ll never part, I\'ll never stray again from you
-        You\'re not alone, oh, and now I\'m where I belong
-        We\'re not alone, oh, I\'ll hold your heart and never let go
-        Everything that I want, I want from you
-        But I just can\'t have you
-        Everything that I need, I need from you
-        But I just can\'t have you
-        You\'re not alone, oh, and now I\'m where I belong
-        We\'re not alone, oh, I\'ll hold your heart and never let go
-        Everything that I want, I want from you
-        But I just can\'t have you
-        Everything that I need, I need from you
-        But I just can\'t have you
-        Everything that I want, I want from you
-        But I just can\'t have you
-        Everything that I need, I need from you
-        But I just, just can\'t have you'
-    ?>
+    <div class="container">
 
-    <p></p>
+        <h1>Stray heart</h1>
+        <h2>Green Day</h2>
+        <!-- Variabile testo canzone -->
+        <?php
+            $stray_heart_lyrics = 'I lost my way, oh baby, this stray heart
+            Went to another, can you recover, baby?
+            Oh, you\'re the only one that I\'m dreamin\' of
+            Your precious heart was torn apart by me and you
+            You\'re not alone, oh, and now I\'m where I belong
+            We\'re not alone, oh, I\'ll hold your heart and never let go
+            Everything that I want, I want from you
+            But I just can\'t have you
+            Everything that I need, I need from you
+            But I just can\'t have you
+            I said a thousand times and now a thousand one
+            We\'ll never part, I\'ll never stray again from you
+            This dog is destined for a home to your heart
+            We\'ll never part, I\'ll never stray again from you
+            You\'re not alone, oh, and now I\'m where I belong
+            We\'re not alone, oh, I\'ll hold your heart and never let go
+            Everything that I want, I want from you
+            But I just can\'t have you
+            Everything that I need, I need from you
+            But I just can\'t have you
+            You\'re not alone, oh, and now I\'m where I belong
+            We\'re not alone, oh, I\'ll hold your heart and never let go
+            Everything that I want, I want from you
+            But I just can\'t have you
+            Everything that I need, I need from you
+            But I just can\'t have you
+            Everything that I want, I want from you
+            But I just can\'t have you
+            Everything that I need, I need from you
+            But I just, just can\'t have you'
+        ?>
+        <!-- Print della variabile contenente il testo della canzone in un <p> -->
+        <p> <?php echo $stray_heart_lyrics ?> </p>
+
+    </div>
 
 </body>
 
